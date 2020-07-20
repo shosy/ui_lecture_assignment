165a7d905f492816c20e5333d2657a7aae2f0b55
shosy <ilikesgh22@gmail.com>
2020-07-20 21:09:51 +0900

    ベストスコアの不具合を修正

--- js/main.js
+++ js/main.js
@@ -11,8 +11,10 @@ function preload() {
 }
 
 function setup() {
-  // localStorage.setItem('best_score', -100000);
-  // localStorage.setItem('best_brc', []);
+  if (localStorage.getItem('best_score') === null) {
+    localStorage.setItem('best_score', -100000);
+    localStorage.setItem('best_brc', []);
+  }
   var canvas = createCanvas(500, 450);
   boundary = new Boundary(); // initialize the boundary including target points on the corners
   score = new Score(); // initialize the scoring system including joint evaluation 
